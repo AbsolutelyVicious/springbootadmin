ebc6c911c6a45b130116282df44f3c7767301bd4
Administrator <17565912@qq.com>
2019-12-08 22:11:54 +0800

    fist commit

---
+++ adminclient/src/test/java/com/springboot/adminclient/AdminclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.adminclient;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
